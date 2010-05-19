7db3569e16272aa5d482491a6dd6b88c4711db82
Michael Wilson <2michaelwilson@gmail.com>
2010-05-19 14:29:26 -0600

    added changes to experiment model

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,2 +1,9 @@
 class Experiment < ActiveRecord::Base
+  attr_accessor :name, :authour
+  attr_accessible :name, :authour
+  
+
+  has_many :steps
+  has_many :definitions
+  has_many :articles
 end
