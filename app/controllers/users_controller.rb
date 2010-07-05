75dab4e5411d2b03b0d87714dae681e13b9fc6a5
Jacqueline Lee <jmasonlee@gmail.com>
2010-07-05 14:12:52 -0600

    fixed some bugs in glossary viewer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,39 @@
 class UsersController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
-  
-before_filter :login_required
-  # render new.rhtml
+
+  before_filter :login_required
+
+  def update
+  @user = User.find(params[:id])
+
+  if @user.update_attributes(params[:user])
+    flash[:notice] = 'User was successfully updated.'
+    redirect_to(user_path(@user))
+  else
+    render :action => 'edit'
+  end
+  end
+
+  def index
+  @users = User.find(:all)
+  end
+
+  def show
+  @user = User.find(params[:id])
+  end
+
+  def destroy
+  @user = User.find(params[:id])
+  @user.destroy
+
+  redirect_to(users_url)
+  end
+
   def new
     @user = User.new
   end
- 
+
   def create
     logout_keeping_session!
     @user = User.new(params[:user])
@@ -26,3 +52,10 @@ before_filter :login_required
     end
   end
 end
+
+def edit
+  @user = User.find(params[:id])
+end
+
+
+ 
