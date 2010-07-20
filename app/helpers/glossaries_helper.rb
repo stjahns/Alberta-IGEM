577fc27747d70e63acea861828531611f9c8169f
Jacqueline Lee <jmasonlee@gmail.com>
2010-07-19 22:19:48 -0600

    fixed a few bugs with the boxes

--- app/helpers/glossaries_helper.rb
+++ app/helpers/glossaries_helper.rb
@@ -55,8 +55,8 @@ module GlossariesHelper
         match = match.sub(/<a href=http:\/\/localhost:3000\/glossaries\/#{id}>/){""}
         
         @def = @def.to_s
-        @def = @def.sub(/<a href=http:\/\/localhost:3000\/glossaries\/#{id}>#{match}<\/a>/){"<a href=http://localhost:3000/glossaries/#{id}>#{match}</a><em class = def>#{definition}</em>"}
-        puts "def is #{@def}"
+        @def = @def.sub(/<a href=http:\/\/localhost:3000\/glossaries\/#{id}>#{match}<\/a>/){"<a href=http://localhost:3000/glossaries/#{id}>#{match}</a><em class = def><div><b>#{term}:</b></div>#{definition}</em>"}
+
         #find a way to not match em's when looking for terms in the string
         #@insert_num += 1
       end
